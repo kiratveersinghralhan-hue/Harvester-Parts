a2feae4093dbfe6cb3ab2c5ab5bb3796520f42a5
kiratveersinghralhan-hue <kiratveersinghralhan@gmail.com>
2026-05-01 20:33:31 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,11 @@
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>self.clients.claim());
+self.addEventListener('install', event => { self.skipWaiting(); });
+self.addEventListener('activate', event => {
+  event.waitUntil((async()=>{
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(() => new Response('', {status: 503, statusText: 'Offline'})));
+});
