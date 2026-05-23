0079f69ba71fba9b5cb3b3edf66b3eaf40f45b3e
kiratveersinghralhan-hue <kiratveersinghralhan@gmail.com>
2026-05-23 19:39:31 +0530

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,13 @@
+// Harvester Parts Supabase + Razorpay config
+// Fill these values after creating your Supabase project.
+window.HP_CONFIG = {
+  SUPABASE_URL: "https://rpsiddurmwtwvpnwzclo.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwc2lkZHVybXd0d3Zwbnd6Y2xvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2ODc5NjMsImV4cCI6MjA5MTI2Mzk2M30.ROGegxjw-HOCkml-5vU19Fd5Bjwn64XpbGIagu8_DMc",
+  RAZORPAY_KEY_ID: "rzp_live_Sq44DIwS74kUQV",
+  OPENAI_ENABLED: false,
+  ADMIN_EMAIL: "kiratveersinghralhan@gmail.com",
+  CURRENCY: "INR",
+  ENABLE_PHONE_OTP: false
+};
+
+
